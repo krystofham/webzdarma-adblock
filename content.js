801d5bbea9d825f3d21c2d07a32d92546ee952d2
krystoham <krystof.ham@volny.cz>
2026-05-05 07:58:58 +0200

    Different approach to hiding ads

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-function hideAds() {
-  const ad1 = document.querySelector('[style*="width: 400px; height: auto; bottom: 5px; position: fixed"]');
-  if (ad1) ad1.style.display = "none";
+function hideAfterFooter() {
+  const footer = document.querySelector('footer');
+  if (!footer) return;
 
-  const ad2 = document.querySelector('[style*="clear:both;width:100%;height:90px"]');
-  if (ad2) ad2.style.display = "none";
+  let el = footer.nextElementSibling;
+  while (el) {
+    el.style.display = "none";
+    el = el.nextElementSibling;
+  }
 }
 
-// Spustit hned i po načtení stránky
-hideAds();
-document.addEventListener("DOMContentLoaded", hideAds);
+hideAfterFooter();
+document.addEventListener("DOMContentLoaded", hideAfterFooter);
